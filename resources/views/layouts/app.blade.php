c3013e6e1d7a8bb84052eda06e525ebed5c0e576
thecaracter <thecaracter1@gmail.com>
2025-01-13 05:37:24 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'SIDESPIN'))</title>
+    <!-- Simple Favicon -->
+    <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Simple Favicon -->
+    <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
     <title>@yield('title', config('app.name', 'Laravel'))</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
